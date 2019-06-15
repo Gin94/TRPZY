2ef250345c570b76d631ba590b307ef20561a3eb
Gin94 <50121053+Gin94@users.noreply.github.com>
2019-06-14 23:17:16 -0400

    perfil
    diseño perfiles

---
+++ perfil.php
@@ -0,0 +1,111 @@
+
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <title>Treepezy, tu viaje soñado</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link rel="stylesheet" href="perfil.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+    <script src="jquery.js"></script>
+  </head>
+  <body>
+<div class="nav">
+      <div class="logo"></div>
+</div>
+
+
+<!----INICIO DEL CONTENEDOR-->
+<div class="contenedor">
+      <div class="opciones">
+        <img src="https://image.freepik.com/iconos-gratis/feliz-cara-cuadrada-emoticono-sonriente-con-gafas-de-sol_318-58589.jpg" class="foto">
+        <h1 style="text-align: center;">Nombre del usuario</h1>
+        <hr style="margin: 10px;">
+        <input type="button" value="Mi Informacion" class="btn">
+
+        <div class="btn" id="btn-1">
+          <h3>Mi informacion</h3>
+        </div>
+        <div class="btn" id="btn">
+          <h3>Mis viajes</h3>
+        </div>
+        <div class="btn">
+          <h3>Mis viajes en espera</h3>
+        </div>
+        <div class="btn">
+          <h3>Planificar</h3>
+        </div>
+        <div class="btn">
+          <h3>Configuración/Ajustes</h3>
+        </div>
+        <div class="btn">
+          <h3>Publicar</h3>
+        </div>
+  
+      </div>
+      <div class="info">
+        <h3>ACA SE MUESTRA TU INFORMACION</h3>
+        <hr style="margin:10px;">
+        <div class="info.user">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+
+
+</div><!--final contenedor-->
+     
+
+<div class="footer">
+      <div class="footer_box">
+        <center>
+        <div class="footer_logo"></div>
+        </center>
+        <div class="fl_txt">Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Quod rerum voluptas
+          alias id possimus cupiditate nihil cumque culpa
+          beatae neque</div>
+      </div>
+      <div class="footer_box">
+        <center>
+        <h3>Contáctenos</h3>
+        <br><br>
+        <a href="">Whastapp</a>
+        <br><br>
+        <a href="">Facebook</a>
+        <br><br>
+        <a href="">Twitter</a>
+        <br><br>
+        <a href="">Instagram</a>
+        </center>
+      </div>
+      <div class="footer_box">
+        <center>
+        <h3>Navegación</h3>
+        <br><br>
+        <a href="">Inicio</a>
+        <br><br>
+        <a href="">Destinos</a>
+        <br><br>
+        <a href="">Destinos de Usuarios</a>
+        </center>
+      </div>
+      <div class="footer_box">
+        <center>
+        <h3>Soporte</h3>
+        <br><br>
+        <a href="">Preguntas Frecuentes</a>
+        <br><br>
+        <a href="">Contacto Desarrollador</a>
+        <br><br>
+        <a href="">Reportar un Error</a>
+        </center>
+      </div>
+      </div>
+  </body>
+</html>
